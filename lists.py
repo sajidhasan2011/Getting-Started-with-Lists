f4bd203e9b30ba49a79f073f9bcb8864acabe775
Sajid <afrojaakter874@gmail.com>
2024-09-11 19:38:12 +0600

    first commit

---
+++ lists.py
@@ -0,0 +1,13 @@
+lst = ["Afnan","Safwan","Sajid","Giga"]
+
+print (lst)
+
+print(lst[0])
+print(lst[1])
+print(lst[2])
+
+numbers =[1,2,3,4,5]
+print(numbers[-2])
+
+aList = [100,200,300,400,500]
+print( aList[::-1] )
